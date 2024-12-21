919fa645f909b4c6db4c10b0ea4495eeb4053989
abdellahcodes <ayoujil.abdellah3@gmail.com>
2024-12-21 17:56:50 +0100

    add SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'SplashScreen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),);
   }
 }
 
